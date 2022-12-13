739aee3ecce1d558c0d9e6e89f471abdf6cf535c
Andrey Anoshkin <a.anoshkin@eapteka.ru>
2022-12-13 12:22:46 +0300

    issue-177

--- MapKitDemo/SearchViewController.swift
+++ MapKitDemo/SearchViewController.swift
@@ -10,21 +10,21 @@ class SearchViewController: BaseMapViewController, YMKMapCameraListener {
 
     var searchManager: YMKSearchManager?
     var searchSession: YMKSearchSession?
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         searchManager = YMKSearch.sharedInstance().createSearchManager(with: .combined)
-        
+
         mapView.mapWindow.map.addCameraListener(with: self)
-        
+
         mapView.mapWindow.map.move(with: YMKCameraPosition(
             target: YMKPoint(latitude: 59.945933, longitude: 30.320045),
             zoom: 14,
             azimuth: 0,
             tilt: 0))
     }
-    
+
     func onCameraPositionChanged(with map: YMKMap,
                                  cameraPosition: YMKCameraPosition,
                                  cameraUpdateReason: YMKCameraUpdateReason,
@@ -37,26 +37,22 @@ class SearchViewController: BaseMapViewController, YMKMapCameraListener {
                     self.onSearchError(error!)
                 }
             }
-            
+
             searchSession = searchManager!.submit(
-                withText: "cafe",
+//                withText: "Россия, Москва, Мосфильмовская улица", // returns true in getAddressMapModelIsEmptyHouse()
+                withText: "Россия, Москва, Центральный административный округ, Пресненский район, Московский международный деловой центр Москва-Сити", // returns false in getAddressMapModelIsEmptyHouse()
                 geometry: YMKVisibleRegionUtils.toPolygon(with: map.visibleRegion),
                 searchOptions: YMKSearchOptions(),
                 responseHandler: responseHandler)
         }
     }
-    
+
     func onSearchResponse(_ response: YMKSearchResponse) {
         let mapObjects = mapView.mapWindow.map.mapObjects
         mapObjects.clear()
-        for searchResult in response.collection.children {
-            if let point = searchResult.obj?.geometry.first?.point {
-                let placemark = mapObjects.addPlacemark(with: point)
-                placemark.setIconWith(UIImage(named: "SearchResult")!)
-            }
-        }
+        debugPrint("TEST METADATA:", response.collection.children.getAddressMapModelIsEmptyHouse())
     }
-    
+
     func onSearchError(_ error: Error) {
         let searchError = (error as NSError).userInfo[YRTUnderlyingErrorKey] as! YRTError
         var errorMessage = "Unknown error"
@@ -65,10 +61,22 @@ class SearchViewController: BaseMapViewController, YMKMapCameraListener {
         } else if searchError.isKind(of: YRTRemoteError.self) {
             errorMessage = "Remote server error"
         }
-        
+
         let alert = UIAlertController(title: "Error", message: errorMessage, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-        
+
         present(alert, animated: true, completion: nil)
+    }
+}
+
+extension Array where Element == YMKGeoObjectCollectionItem {
+    func getAddressMapModelIsEmptyHouse() -> Bool {
+        for item in self {
+            if let _ = item.obj?.metadataContainer
+                .getItemOf(YMKSearchToponymObjectMetadata.self) as? YMKSearchToponymObjectMetadata {
+                return true
+            }
+        }
+        return false
     }
 }
